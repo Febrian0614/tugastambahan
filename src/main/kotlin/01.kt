2e33cc0148f35b2fcb7d78a3a9025abf8dfff75a
Febrian0614 <febrian.abirin123@gmail.com>
2023-09-25 08:38:14 +0700

    tugastambahan

---
+++ src/main/kotlin/01.kt
@@ -0,0 +1,42 @@
+const val App = "Haruka"
+
+fun main() {
+    //Tipe Data String
+
+    var a: String = "Haruka"
+    var b: String = "Febrian"
+    var c: String = "saya"
+    var d: String = "$a $b"
+    println(d)
+
+    var e: String = """ 
+        Rahma
+        Aulia
+        Putri
+    """.trimIndent()
+    println(e)
+
+    //Tipe data Character
+    println(a[0])
+    println(a[1])
+    println(a[2])
+    println(a[3])
+    println(a[4])
+    println(a[5])
+
+    //variabel pada kotlin
+    println("dia bernama $App")
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
